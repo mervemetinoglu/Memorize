0627c425a339cd06f11f6584637e813c8fb12f58
mervemetinoglu <mervemetinogluu@gmail.com>
2024-01-31 16:32:32 +0300

    remove card view

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,10 +12,13 @@ struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
 
     private let aspectRatio: CGFloat = 2 / 3
+    private let spacing: CGFloat = 4
 
     var body: some View {
         VStack {
-            cards.animation(.default, value: viewModel.cards)
+            cards
+                .foregroundStyle(viewModel.color)
+                .animation(.default, value: viewModel.cards)
             Button("Shuffle") {
                 viewModel.shuffle()
             }
@@ -25,34 +28,12 @@ struct EmojiMemoryGameView: View {
 
     private var cards: some View {
         AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
-            CardView(card: card)
-                .padding(4)
+            CardView(card)
+                .padding(spacing)
                 .onTapGesture {
                     viewModel.choose(card)
                 }
         }
-        .foregroundColor(.red)
-    }
-}
-
-struct CardView: View {
-    let card: EmojiMemoryGame.Card
-
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: 20)
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 3)
-                Text(card.content)
-                    .font(.system(size: 200))
-                    .minimumScaleFactor(0.01)
-                    .aspectRatio(1, contentMode: .fit)
-            }
-            .opacity(card.isFaceUp ? 1 : 0)
-            base.fill().opacity(card.isFaceUp ? 0 : 1)
-        }
-        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
